a2048252843f1c145abe8b482b1d6a0aac78578c
wx <2214380963@qq.com>
2022-08-24 15:31:23 +0800

    list

--- src/views/home/components/hot-list-item-v1.vue
+++ src/views/home/components/hot-list-item-v1.vue
@@ -10,15 +10,51 @@ defineProps({
 <template>
   <div class="item">
     <img :src="house.image.url" />
+    <dic class="describe">
+      <div class="summaryText">
+        <span>{{ house.summaryText }}</span>
+      </div>
+      <div class="houseName">
+        <span>{{ house.houseName }}</span>
+      </div>
+      <div class="core-price">
+        <span class="core"><van-rate v-model="house.commentScore" color="orange" size="13px" /></span>
+        <span class="price">￥{{ house.finalPrice }}</span>
+      </div>
+    </dic>
   </div>
 </template>
 
 <style scoped lang="less">
 .item {
+  position: relative;
   overflow: hidden;
+  border-radius: 5px;
+  color: white;
+  background-color: white;
   img {
     width: 200px;
     height: 250px;
+  }
+  .describe {
+    padding: 10px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    .houseName {
+      margin: 5px 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+    }
+    .core-price {
+      display: flex;
+      justify-content: space-between;
+    }
   }
 }
 </style>
--- src/views/home/components/hot-list-item-v2.vue
+++ src/views/home/components/hot-list-item-v2.vue
@@ -10,15 +10,78 @@ defineProps({
 <template>
   <div class="item">
     <img :src="house.image.url" />
+    <div class="describe">
+      <div class="location">
+        <span>
+          <van-icon name="location-o" />
+          {{ house.location }}
+        </span>
+      </div>
+      <div class="houseName">
+        <span>{{ house.houseName }}</span>
+      </div>
+      <div class="summaryText">
+        <span>{{ house.summaryText }}</span>
+      </div>
+      <div class="price">
+        <span class="finalPrice">￥{{ house.finalPrice }}</span>
+        <span class="productPrice">￥{{ house.productPrice }}</span>
+        <span class="priceTipBadge">{{ house.priceTipBadge.text }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="less">
 .item {
   overflow: hidden;
+  box-shadow: 0 0 10px 1px rgb(238, 238, 238);
+  border-radius: 5px;
+  height: 250px;
   img {
     width: 200px;
-    height: 250px;
+    height: 130px;
+  }
+  .describe {
+    padding: 10px;
+    .location {
+      color: #999999;
+      font-size: 13px;
+    }
+    .houseName {
+      margin: 5px 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+    }
+    .summaryText {
+      color: #999999;
+      font-size: 13px;
+    }
+    .price {
+      display: flex;
+      margin-top: 5px;
+      align-items: flex-start;
+      .finalPrice {
+        color: #fa705e;
+      }
+      .productPrice {
+        font-size: 12px;
+        text-decoration: line-through;
+        color: #999999;
+        margin: 0 8px 0 0;
+      }
+      .priceTipBadge {
+        background-color: #e98f84;
+        font-size: 12px;
+        padding: 2px 8px;
+        text-align: center;
+        border-radius: 10px;
+        color: white;
+      }
+    }
   }
 }
 </style>
